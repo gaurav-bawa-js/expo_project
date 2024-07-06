fb444df43f95aae2efaed8bad744346bb49a3eba
bawa <gbawa0609@gmail.com>
2024-07-06 20:18:11 +0530

    Initial commit native starter template

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View className='flex-1 items-center justify-center bg-slate-600'>
+      <Text className='text-slate-800'>Styling just works! 🎉</Text>
+    </View>
+  )
+}
+
+export default App
